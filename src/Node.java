b1f81053d142eff19118ea28c2520460ec7e0fe9
Pinguinmann <Tom.Aussenhofer@gmx.de>
2014-08-21 18:25:54 +0200

    okay

---
+++ src/Node.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Vector;
+
+import ogl.vecmath.Matrix;
+
+
+public class Node {
+
+
+	private Vector<Node> childNodes = new Vector<Node>();
+	private String name;
+	private Matrix transformation;
+	
+	public void setTransformation(Matrix transformation) {
+		this.transformation = transformation;
+	}
+	
+	public Matrix getTransformation() {
+		return transformation;
+	}
+	
+	public void addNode(Node node){
+		childNodes.add(node);
+	}
+	
+	public void removeNode(Node node){
+		childNodes.remove(node);
+	}
+	
+	@Override
+	public String toString() {
+		return "Node: " + name;
+	}
+	
+}
