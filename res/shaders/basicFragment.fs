d3522fb36c4bc270bb7a41447cd5b87ba52265e0
Pinguinmann <Tom.Aussenhofer@gmx.de>
2014-09-03 14:02:42 +0200

    GLSL fertig

--- res/shaders/basicFragment.fs
+++ res/shaders/basicFragment.fs
@@ -1,8 +1,14 @@
 #version 330
 
+//cbC
+in vec4 colorQ;
+
 out vec4 fragColor;
 
 void main()
 {
-	fragColor = vec4(0.0, 1.0, 1.0, 1.0);
+	//cbC
+	fragColor = colorQ;
+	//cbU
+	//fragColor = vec4(0.0, 1.0, 1.0, 1.0);
 }
