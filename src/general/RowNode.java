468dcdfb54d185e2e67325b453a154e8782fc778
GrosserStuhl <paralysekid@gmail.com>
2014-09-12 19:30:12 +0200

    nnn

--- src/general/RowNode.java
+++ src/general/RowNode.java
@@ -7,6 +7,8 @@ import static ogl.vecmathimp.FactoryDefault.vecmath;
 public class RowNode extends Node {
 
 	private int rowIndex;
+	
+	//ddd
 
 	public RowNode(int rowIndex) {
 		this.rowIndex = rowIndex;
